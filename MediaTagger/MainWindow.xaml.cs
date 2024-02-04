a6b279f24f835dbdf3ab3abecda91a0e014da404
Jacob Romkey <Jacobromkey@icloud.com>
2024-02-04 17:14:35 -0400

    added taglib

--- MediaTagger/MainWindow.xaml.cs
+++ MediaTagger/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +22,34 @@ namespace MediaTagger
     /// </summary>
     public partial class MainWindow : Window
     {
+        TagLib.File currentFile;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            //instantiating an OpenFileDialog
+            OpenFileDialog fileDlg = new OpenFileDialog();
+
+            //Create a file filter
+            fileDlg.Filter = "MP3 files (*.MP3)|*.MP3 | All files (*.*)|*.*";
+
+            //ShowDialog shows onscreen for the user
+            //By default it return true if the user selects a file and hits "Open"
+            if (fileDlg.ShowDialog() == true)
+            {
+                //The filename property stores the full path that was selected
+                fileNameBox.Text = fileDlg.FileName;
+
+                //Example of creating a TagLib file object, for accessing mp3 metadata
+                currentFile = TagLib.File.Create(fileDlg.FileName);
+
+                //Set the source of the media player element.
+                myMediaPlayer.Source = new Uri(fileDlg.FileName);
+            }
         }
     }
 }
--- MediaTagger/obj/Release/net8.0-windows/MediaTagger.AssemblyInfo.cs
+++ MediaTagger/obj/Release/net8.0-windows/MediaTagger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaTagger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caaae4dd9338a30334554c69d3090ce426ab24e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96fada20fae06c68461c768bc290ad1c62aede31")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaTagger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaTagger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
